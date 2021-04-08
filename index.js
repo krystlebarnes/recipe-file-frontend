61659c15ece889f1e92ec4e56f93512774ec235c
github username <github email address>
2021-04-08 17:21:18 -0600

    added new recipe form and form handler function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const endPoint = "http://localhost:3000/api/v1//recipes"
 
 document.addEventListener('DOMContentLoaded', () => {
     getRecipes()
+
+    const createRecipeForm = document.querySelector('#create-recipe-form')
+    createRecipeForm.addEventListener('submit', (e) => createFormHandler(e))
 })
 
 function getRecipes() {
@@ -23,3 +26,12 @@ function getRecipes() {
         })
     })
 }
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector('#input-name').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
+    const mealTypeId = parseInt(document.querySelector('#meal_type').value)
+    postRecipe(nameInput, descriptionInput, imageInput, mealTypeInput)
+  }
